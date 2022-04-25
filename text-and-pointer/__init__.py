c375c4ec8da7bcf0eac4db7e3db757c3221e1717
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2022-04-25 12:32:50 +0800

    added new problem/lab (week 4)

---
+++ text-and-pointer/__init__.py
@@ -0,0 +1,32 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """The program and text-pointer.c exists"""
+    check50.exists("text-pointer.c")
+    check50.exists("text-pointer")
+
+@check50.check(exists)
+def compiles():
+    """text-pointer.c compiles"""
+    check50.c.compile("text-pointer.c", lcs50=True)
+
+@check50.check(exists)
+def valgrind_check():
+    """No memory errors"""
+    check50.c.valgrind("./text-pointer").stdin("Hello world").exit(0)
+
+@check50.check(exists)
+def text_pointer():
+    """Prints the text and its pointer"""
+    test = check50.run("./text-pointer")
+    test.stdin("Hello world")
+    test.stdout("The text is Hello world and the pointer is 0x\w{0,32}", regex=True).exit(0)
+
+@check50.check(exists)
+def number_pointer():
+    """Can check and print number and its pointers"""
+    test = check50.run("./text-pointer")
+    test.stdin("84")
+    test.stdout("The text is 84 and the pointer is 0x\w{0,32}", regex=True).exit(0)
