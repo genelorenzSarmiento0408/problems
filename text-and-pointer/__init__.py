375088c7be40a594b20bf3d898695c43b7d0de0d
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2022-04-25 12:34:24 +0800

    Revise from existing to compiling

--- text-and-pointer/__init__.py
+++ text-and-pointer/__init__.py
@@ -12,19 +12,19 @@ def compiles():
     """text-pointer.c compiles"""
     check50.c.compile("text-pointer.c", lcs50=True)
 
-@check50.check(exists)
+@check50.check(compiles)
 def valgrind_check():
     """No memory errors"""
     check50.c.valgrind("./text-pointer").stdin("Hello world").exit(0)
 
-@check50.check(exists)
+@check50.check(compiles)
 def text_pointer():
     """Prints the text and its pointer"""
     test = check50.run("./text-pointer")
     test.stdin("Hello world")
     test.stdout("The text is Hello world and the pointer is 0x\w{0,32}", regex=True).exit(0)
 
-@check50.check(exists)
+@check50.check(compiles)
 def number_pointer():
     """Can check and print number and its pointers"""
     test = check50.run("./text-pointer")
